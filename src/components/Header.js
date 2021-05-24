0029d6f43e283eaf4d0e2d64baccf2703c216015
the-halfbloodprince <ammanishkumardas@gmail.com>
2021-05-24 20:56:14 +0530

    Made the header component
    1. Dumped tailwind due to :
        a. Personal lacking of skills in tailwind
        b. Didn't wanted to mix styles and html together and have them separated
    
    2. Built out rest of the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,60 @@
 import Image from 'next/image'
+import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/solid'
+import styles from '../styles/Header.module.sass'
 
+const tags = [<MenuIcon className={styles.menuIcon} /> ,'All', 'Electronics', 'Mobiles', 'Home & Grocery', 'Buy Again', 'Health and Personal Care', 'Eatables']
 function Header() {
     return (
-        <header className="header">
+        <header className={styles.header} >
 
             {/* Top Nav */}
-            <div>
-                <div>
-                    <Image src="/logo.png" width={150} height={70} objectFit="contain" />
+            <div className={styles.header__bar1} >
+                {/* Logo */}
+                <div className={styles.logoContainer} >
+                    <Image src="/logo.png" width={150} height={60} objectFit="contain" className={styles.logo} />
+                </div>
+                {/* Search */}
+                <div className={styles.search}>
+                    <div className={styles.search__content}>
+                        <input type="text" className={styles.search__input} />
+                        <SearchIcon className={styles.search__icon} />
+                    </div>
+                </div>
+                {/* Nav */}
+                <div className={styles.header__nav} >
+
+                    <div className={styles.header__option} >
+                        <div className={styles.header__optionLine1} >Hello</div>
+                        <div className={styles.header__optionLine2} >Sign In</div>
+                    </div>
+                    <div className={styles.header__option} >
+                        <div className={styles.header__optionLine1} >Returns</div>
+                        <div className={styles.header__optionLine2} >& Orders</div>
+                    </div>
+                    <div className={styles.header__option} >
+                        <div className={styles.header__optionLine1} >Your</div>
+                        <div className={styles.header__optionLine2} >Prime</div>
+                    </div>
+                    <div className={styles.header__optionBasket} >
+                        <ShoppingCartIcon className={styles.cartIcon} />
+                        <span className={styles.header__optionLine2, styles.header__basketCount}>0</span>
+                    </div>
+
                 </div>
             </div>
+
+            {/* Tags */}
+            <div className={styles.header__bar2}>
+            {
+                tags.map((tag, key) => (
+                    <a class={styles.tag, key>4 && styles.hideInSmall} key={key}>{tag}</a>
+                ))
+            }
+            </div>
+
+
+
+
 
         </header>
     )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../styles/globals.sass'
 
 const MyApp = ({ Component, pageProps }) => {
   return (

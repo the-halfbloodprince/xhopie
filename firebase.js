7057b0d2dce96426b1b1dffee5b259dafb9e9126
the-halfbloodprince <ammanishkumardas@gmail.com>
2021-06-10 17:27:33 +0530

    Added firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBYuswRjVHGwTLHXEnak1Q3E4WC8UNRaMs",
+  authDomain: "xhopie-bymkd.firebaseapp.com",
+  projectId: "xhopie-bymkd",
+  storageBucket: "xhopie-bymkd.appspot.com",
+  messagingSenderId: "381523325674",
+  appId: "1:381523325674:web:f30eb457ddfdf276e09d93",
+  measurementId: "G-0Y5JDNY3VL"
+};
+
+const app = !firebase.apps.length ?
+              firebase.initializeApp(firebaseConfig)
+              : firebase.app()
+
+const db = app.firestore()
+
+export default db
